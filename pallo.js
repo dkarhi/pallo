9c63b7105e8dbf983e22b77fa68dd2fe47d67022
David Karhi <dkarhi@gmail.com>
2014-08-15 18:03:18 -0500

    Add initial pass at game board and some of the framework for node

---
+++ pallo.js
@@ -0,0 +1,26 @@
+var handler = function(req, res) {
+  fs.readFile('./pallo.html', function (err, data) {
+    if(err) throw err;
+    res.writeHead(200);
+    res.end(data);
+  }); 
+};
+
+var server = require('http').createServer(handler);
+var io = require('socket.io').listen(server);
+var fs = require('fs');
+var port = 80;
+
+server.listen(port);
+
+io.sockets.on('connection', function (socket) {
+  var user = addUser();
+  updateBoard();
+  socket.emit("Welcome", user);
+  socket.on('disconnect', function () {
+    removeUser(user);
+  });
+  socket.on("click", function() {
+    setGamePiece();
+  });
+});
